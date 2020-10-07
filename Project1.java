4e5eaa3381242ec5b5da01f2970823a7bccf1d66
sammana308 <sammana.kabir.998@my.csun.edu>
2020-10-07 15:14:36 -0700

    Didnt push my print method yesterday for some reason

--- Project1.java
+++ Project1.java
@@ -7,31 +7,15 @@ import java.io.FileReader;
 
 //Main method 
 public class Project1 {
-	
-//	void FileReader(String input)throws IOException {
-//		//Read file with fileReader using BufferedReader
-//		//Providing the file path of the file input1.txt to read texts from
-//			File file = new File("/Users/sammanakabir/Desktop/comp482/Project1/src/input1.txt");
-//			BufferedReader reader = new BufferedReader(new FileReader(file));
-//			String input1;	
-//			StringBuilder holder = new StringBuilder();
-//			while((input1 = reader.readLine())!= null) {
-//			//input = reader.readLine() ;
-//			 holder.append(input1);   //placing the input from the file to the String holder
-//			 System.out.println(input1);
-//			 holder.append(" ");     //placing space between string 
-//			}
-//			 System.out.println(holder);
-//		     String string = holder.toString();
-//	}
-//	void Array_StringtoInt(string[]) {
-//		String[] ArrayStr = string.split(" "); 
-//        int[] IntArray = new int[ArrayStr.length];
-//        for(int i = 0; i < ArrayStr.length; i++) {
-//        	(IntArray[i]) = Integer.parseInt(ArrayStr[i]);
-//        	System.out.print(IntArray[i] + " ");
-//        }	
-//	}
+	//Method for accessing Integer Arrays outside of their for loop
+	//and for printing
+	public static void printArray(int arr[]){
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 	public static void main(String args[])throws IOException {
 	//Read file with fileReader using BufferedReader
 	//Providing the file path of the file input1.txt to read texts from
